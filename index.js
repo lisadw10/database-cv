037c0c26dae1bc952d8fbb535094292b3188afe2
lisadw10 <lisadwiwahyuni1008@gmail.com>
2022-06-29 15:25:45 +0700

    add proccess port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Connection URL
 const url =
